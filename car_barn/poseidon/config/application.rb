9dd437ee531e5fafb3ad417b6bae0e37f74e13aa
kevin Hinton <kevin.joey89@gmail.com>
2019-08-29 19:11:24 -0500

    skeleton app
    Skeleton app generated to house all of our changes.

---
+++ car_barn/poseidon/config/application.rb
@@ -0,0 +1,16 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Poseidon
+  class Application < Rails::Application
+    config.load_defaults 5.1
+    console do
+      ActiveRecord::Base.connection
+    end
+  end
+end
